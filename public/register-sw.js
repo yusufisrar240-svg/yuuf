3736503c8f001e75db162f94d11b050b8d05967c
yusufisrar240-svg <yusufisrar240@gmail.com>
2026-09-05 21:04:19 -0400

    Add service worker registration

---
+++ public/register-sw.js
@@ -0,0 +1,10 @@
+"use strict";
+const stockSW = "/uv/sw.js";
+const swAllowedHostnames = ["localhost", "127.0.0.1"];
+async function registerSW() {
+  if (!navigator.serviceWorker) {
+    if (location.protocol !== "https:" && !swAllowedHostnames.includes(location.hostname)) throw new Error("Service workers cannot be registered without https.");
+    throw new Error("Your browser doesn't support service workers.");
+  }
+  await navigator.serviceWorker.register(stockSW);
+}
