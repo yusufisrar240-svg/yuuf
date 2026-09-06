c2f6c0df841e91dc54bdec6c5e1d151c541fe505
yusufisrar240-svg <yusufisrar240@gmail.com>
2026-09-05 21:04:23 -0400

    Add search helper

---
+++ public/search.js
@@ -0,0 +1,6 @@
+"use strict";
+function search(input, template) {
+  try { return new URL(input).toString(); } catch (err) {}
+  try { const url = new URL(`http://${input}`); if (url.hostname.includes(".")) return url.toString(); } catch (err) {}
+  return template.replace("%s", encodeURIComponent(input));
+}
